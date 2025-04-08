a29d98332a3297f8ed85c5df955bd5fbfba17455
Pelutson <marc.miehe@icloud.com>
2025-04-08 15:24:45 +0200

    v1

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
 
           <h1>Hi, my name ist <span style={{color:"#518af5"}}>Marc</span></h1>
           <p>Some of my project are made in <span>Blender</span></p>
+          <h2>by</h2>
 
         </div>
         
